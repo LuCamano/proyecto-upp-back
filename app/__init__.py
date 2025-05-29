c2281a52900c7de5f6ac4269b196ac43971670f2
Luciano <lu.camano@duocuc.cl>
2025-05-28 20:07:53 -0400

    Agregar archivos iniciales para la aplicación FastAPI, incluyendo configuración de base de datos y rutas

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from routes import router
+from .db import create_db_and_tables
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Code to run at startup
+    create_db_and_tables()
+    yield
+    # Code to run at shutdown
+
+def create_app() -> FastAPI:
+    app = FastAPI(lifespan=lifespan)
+    # Register routers
+    app.include_router(router, prefix="/api/v1", tags=["api"])
+    
+    return app
+
+app = create_app()
+
+@app.get("/")
+async def root():
+    return {"message": "Hola, mundo!"}
---
+++ run.py
@@ -0,0 +1,11 @@
+## Antes de ejecutar el resto de la aplicación, es importante cargar las variables de entorno
+from dotenv import load_dotenv
+load_dotenv()
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    from uvicorn import run
+    run(app, host="0.0.0.0", port=8000, log_level="info")
