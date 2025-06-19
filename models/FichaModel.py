1ed4f2b7ecd70c7fc12097c64a199f5dd15d64b2
Luciano <lu.camano@duocuc.cl>
2025-06-18 21:01:35 -0400

    Actualizar el modelo FichaBase para incluir el tipo datetime en fecha_envio y agregar la importación de TIMESTAMP.

--- models/FichaModel.py
+++ models/FichaModel.py
@@ -1,7 +1,7 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, TIMESTAMP
 from typing import Optional
 from uuid import UUID
-from datetime import date
+from datetime import date, datetime
 
 class FichaBase(SQLModel):
     estudiante_id: int = Field(foreign_key="estudiante.id", description="ID del estudiante asociado a la ficha", nullable=False)
@@ -9,4 +9,4 @@ class FichaBase(SQLModel):
     cupo_id: int = Field(foreign_key="cupo.id", description="ID del cupo asociado a la ficha", nullable=False)
     fecha_inicio: date = Field(description="Fecha de inicio de la ficha", nullable=False)
     fecha_termino: Optional[date] = Field(default=None, description="Fecha de término de la ficha", nullable=True)
-    fecha_envio: date = Field(description="Fecha de envío de la ficha al estudiante", nullable=False)
+    fecha_envio: datetime = Field(description="Fecha de envío de la ficha al estudiante", sa_type=TIMESTAMP, nullable=False)
